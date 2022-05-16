7d9860bb9cd55e29f86d16cb2fe42f67bcc9a1e5
baiyastan <bsbaha195@gmail.com>
2022-05-16 17:13:21 +0600

    add may

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,13 +1,16 @@
-import Navbar from './Navbar'
-import Digital from './Digital'
-import '../Styles/components/Main.scss'
+import Navbar from "./Navbar";
+import Digital from "./Digital";
+import "../Styles/components/Main.scss";
+import Sheet from "./Sheet";
 function Main() {
-    return (
-        <div className="container">
-            <Navbar/><br/>
-            <Digital />
-        </div>
-    )
+  return (
+    <div className="container">
+      <Navbar />
+      <br />
+      <Digital />
+      <Sheet />
+    </div>
+  );
 }
 
 export default Main;
---
+++ src/Components/Sheet.js
@@ -0,0 +1,43 @@
+import "../Styles/components/Sheet.scss";
+function Sheet() {
+  return (
+    <div className="sheets">
+      <div></div>
+      <div className="sheet-containers">
+        <div className="line"></div>
+        <div className="november text">November 2017</div>
+        <div className="march text">March 2018</div>
+        <div className="may text">May 2020</div>
+        <div className="January text">January 2018</div>
+        <div className="March text">March 2019</div>
+        <div className="September text">September 2020</div>
+        <div className="track black-text">
+          Tracking services for different smart contract types
+        </div>
+        <div className="release black-text">
+          Release of a decentralized app for affiliate networks
+        </div>
+        <div className="Development black-text">
+          Development of different types of smart contracts (CPA, CPL, CPC, CPS,
+          CPI)
+        </div>
+        <div className="release2 black-text">
+          Release of a decentralized app for merchants / affiliates
+        </div>
+        <div className="Launch black-text">
+          Launch of the world's first decentralized affiliate network based on
+          the HOQU platform
+        </div>
+
+        <div className="one crug"></div>
+        <div className="two crug"></div>
+        <div className="three crug"></div>
+        <div className="four crug"></div>
+        <div className="five crug1"></div>
+        <div className="sex crug1"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Sheet;
