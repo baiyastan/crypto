4066384309f9326db395919ebe8a278f0ca96631
baiyastan <bsbaha195@gmail.com>
2022-05-16 11:29:47 +0600

    first commit

---
+++ src/Components/Main.js
@@ -0,0 +1,11 @@
+import Navbar from './Navbar'
+import '../Styles/components/Main.scss'
+function Main() {
+    return (
+        <div className="container">
+            <Navbar/>
+        </div>
+    )
+}
+
+export default Main;
